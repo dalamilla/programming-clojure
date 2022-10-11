354334ea0226da2b58cd1a72aeb45d73412ed13f
Daniel Alamilla <daniel.alamilla.co@gmail.com>
2022-10-11 08:33:13 -0500

    add euler template project with lein

---
+++ euler/project.clj
@@ -0,0 +1,7 @@
+(defproject euler "0.1.0"
+  :description "A series of challenging mathematical/computer programming problems."
+  :url "https://projecteuler.net/"
+  :license {:name "The MIT License"
+            :url "http://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :repl-options {:init-ns euler.core})
